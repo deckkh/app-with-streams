36dd6ed09fdaa5df003618b4dfa8c32cbb35ccb7
Kim Kruse Hansen <deckkh@hotmail.com>
2021-08-16 18:38:38 +0200

    refactor

--- app/src/main/java/app/with/streams/App.java
+++ app/src/main/java/app/with/streams/App.java
@@ -40,55 +40,55 @@ public class App {
 
           System.out.println(user);
 
+          // the builder is used to construct the topology
+          StreamsBuilder builder = new StreamsBuilder();
+
+          UserSerdes userSerdes = new UserSerdes();
+          LoginSerdes loginSerdes = new LoginSerdes();
+          VoidSerde voidSerde = new VoidSerde();
+
+          // read from the source topic, "users"
+          KStream<Void, User> stream = builder.stream(prop.getPropValue("stream-users"),Consumed.with(voidSerde, userSerdes));
+          KStream<Void, Login> login = builder.stream(prop.getPropValue("stream-login"),Consumed.with(voidSerde, loginSerdes));
+
+          // for each record that appears in the source topic,
+          // print the value
+          stream.foreach(
+              (key, value) -> {
+                System.out.println("(User) Hello, " + value);
+              });
+
+              login.foreach(
+                  (key, value) -> {
+                    System.out.println("(Login) Hello, " + value);
+                  });
+          
+          // you can also print using the `print` operator
+          // stream.print(Printed.<String, String>toSysOut().withLabel("source"));
+
+          // set the required properties for running Kafka Streams
+          Properties config = new Properties();
+          config.put(StreamsConfig.APPLICATION_ID_CONFIG, prop.getPropValue("app-dev-id"));
+          config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, prop.getPropValue("kafka-uri"));
+          config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+          config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Void().getClass());
+          config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+          // build the topology and start streaming
+          KafkaStreams streams = new KafkaStreams(builder.build(), config);
+          streams.start();
+
+          // close Kafka Streams when the JVM shuts down (e.g. SIGTERM)
+          Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+          
+              logger.debug("Debug: Program has finished successfully");
+              logger.error("Error: Program has errors");
 
         } catch (IOException e) {
           // TODO Auto-generated catch block
           e.printStackTrace();
         }
 
+      }
 
-    // the builder is used to construct the topology
-    StreamsBuilder builder = new StreamsBuilder();
-
-    UserSerdes userSerdes = new UserSerdes();
-    LoginSerdes loginSerdes = new LoginSerdes();
-    VoidSerde voidSerde = new VoidSerde();
-
-    // read from the source topic, "users"
-    KStream<Void, User> stream = builder.stream("users",Consumed.with(voidSerde, userSerdes));
-     KStream<Void, Login> login = builder.stream("login",Consumed.with(voidSerde, loginSerdes));
-
-    // for each record that appears in the source topic,
-    // print the value
-    stream.foreach(
-        (key, value) -> {
-          System.out.println("(User) Hello, " + value);
-        });
-
-         login.foreach(
-             (key, value) -> {
-               System.out.println("(Login) Hello, " + value);
-             });
-    
-    // you can also print using the `print` operator
-    // stream.print(Printed.<String, String>toSysOut().withLabel("source"));
-
-    // set the required properties for running Kafka Streams
-    Properties config = new Properties();
-    config.put(StreamsConfig.APPLICATION_ID_CONFIG, "dev1");
-    config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "172.26.148.42:29092");
-    config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-    config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Void().getClass());
-    config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-
-    // build the topology and start streaming
-    KafkaStreams streams = new KafkaStreams(builder.build(), config);
-    streams.start();
-
-    // close Kafka Streams when the JVM shuts down (e.g. SIGTERM)
-    Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-    
-        logger.debug("Debug: Program has finished successfully");
-        logger.error("Error: Program has errors");
-    }
 }
